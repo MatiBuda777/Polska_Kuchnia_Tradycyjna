815048e0a73ac8677a88662b1ef9f3d423dba9d5
mateuszbudek <mateusz.budek@cyfroweszkoly.pl>
2025-11-19 12:30:56 +0100

    Add binding and actions

--- app/src/main/java/com/example/polska_kuchnia_tradycyjna/MenuChoiceFragment.kt
+++ app/src/main/java/com/example/polska_kuchnia_tradycyjna/MenuChoiceFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.polska_kuchnia_tradycyjna.databinding.FragmentMenuChoiceBinding
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -14,7 +17,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MenuChoiceFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    private var _binding: FragmentMenuChoiceBinding? = null
+    private val binding get() = _binding!!
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -26,12 +31,28 @@ class MenuChoiceFragment : Fragment() {
         }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_choice, container, false)
+        _binding = FragmentMenuChoiceBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonReadyMeals.setOnClickListener {
+            findNavController().navigate(R.id.action_menuChoiceFragment_to_readyMealFragment)
+        }
+        binding.buttonCustomMeals.setOnClickListener {
+            findNavController().navigate(R.id.action_menuChoiceFragment_to_customMealFragment)
+        }
     }
 
     companion object {
